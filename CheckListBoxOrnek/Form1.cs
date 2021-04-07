755b51adb32838ef71e76e9e1be4d157ed0d80be
abdulkaharr <60811105+abdulkaharr@users.noreply.github.com>
2021-04-07 18:29:28 +0300

    CheckListBoxOrnek

---
+++ CheckListBoxOrnek/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckListBoxOrnek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+  
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            int secilenicerik = e.Index; //seçilen indexi secileniceriğe atık
+            string isaretlenenmetin = checkedListBox1.Items[secilenicerik].ToString();// Burada seçilen indexin karşılığı olan kelimeyi isaretlenenmetine atıyoruz 
+
+            if (listBox1.Items.IndexOf(isaretlenenmetin) > -1)//kullanan kişi check box yerine beşka bir yere bastığı zaman 0 dan küçük olacağı için buraya düşüyor
+            {
+                MessageBox.Show("bastığınız yerde hiç bir seçenek bulunmamaktadır!");
+                return;
+            }
+
+           if ( checkedListBox1.GetItemChecked(secilenicerik) == false) //GetItemChecked getir alamında kullanılıyor false eşitlememizin sebebi GetItemChecked kendine has özeliğidir
+            {
+                listBox1.Items.Add(isaretlenenmetin);
+            }
+
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Kodlar sade ve anlaşılır.
+kodların yan tarafında yorum satırı ile açıklaması mevcuttur.
